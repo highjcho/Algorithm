a4dc7790c2af656fda8b30e78f657253407bd486
highcho <highjcho@gmail.com>
2022-07-23 00:31:59 +0900

    [SOLVED] Data Structure - baekjoon 1655

---
+++ 3_data_structure/baekjoon_1655_queue.py
@@ -0,0 +1,17 @@
+import sys, heapq
+
+input = sys.stdin.readline
+n = int(input())
+left = []
+right = []
+for i in range(n):
+	num = int(input())
+	if len(left) == len(right):
+		heapq.heappush(left, -num)
+	else:
+		heapq.heappush(right, num)
+	if right and -left[0] > right[0]:
+		heapq.heappush(left, -heapq.heappop(right))
+		heapq.heappush(right, -heapq.heappop(left))
+	print(-left[0])
+	
