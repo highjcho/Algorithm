5223fed883eafccfa77df4b12a3a1752d46c6434
highcho <highjcho@gmail.com>
2024-01-15 09:04:18 +0900

    programmers_할인 행사

---
+++ 7_sliding_window/programmers_할인_행사.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        
+        Map<String, Integer> bag = new HashMap<>();
+        for (int i = 0; i < want.length; i++)
+            bag.put(want[i], number[i]);
+        for (int i = 0; i < 10; i++) {
+            if (bag.containsKey(discount[i])) {
+                String obj = discount[i];
+                bag.put(obj, bag.get(obj) - 1);
+            }
+        }
+        if (Collections.max(bag.values()) <= 0)
+            answer++;
+        for (int i = 0, j = 10; j < discount.length; i++, j++) {
+            String obj1 = discount[i], obj2 = discount[j];
+            if (bag.containsKey(obj1))
+                bag.put(obj1, bag.get(obj1) + 1);
+            if (bag.containsKey(obj2))
+                bag.put(obj2, bag.get(obj2) - 1);
+            if (Collections.max(bag.values()) <= 0)
+                answer++;
+        }
+        return answer;
+    }
+}
